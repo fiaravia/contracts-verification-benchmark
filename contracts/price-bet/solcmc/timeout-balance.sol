ab2108c4e35599a29973038f1f2edb9405466f9d
Massimo Bartoletti <bart@unica.it>
2025-09-02 11:39:11 +0200

    price-bet: timeout-balance*

--- contracts/price-bet/solcmc/timeout-balance.sol
+++ contracts/price-bet/solcmc/timeout-balance.sol
@@ -3,6 +3,7 @@
 /// @custom:preghost function timeout
 uint prev_contract_balance = address(this).balance;
 uint prev_owner_balance = address(owner).balance;
+require (owner != address(this));
 
 /// @custom:postghost function timeout
 assert (address(owner).balance == prev_owner_balance + prev_contract_balance);
